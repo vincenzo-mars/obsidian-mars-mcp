c0e2463c8e69ba16e04f982b364be06abe0fed23
vincenzo-mars <vincenzo.marsiano@thewavestudio.it>
2026-03-20 10:09:12 +0100

    feat(package): renamed obsiadian-mcp to obsidian-mars-mcp

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 import pkg from "../package.json" with { type: "json" };
 
-export const MCP_SERVER_NAME = "obsidian-mcp";
+export const MCP_SERVER_NAME = pkg.name;
 export const MCP_SERVER_VERSION: string = pkg.version;
